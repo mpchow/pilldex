352cabfefcd6a08fe5d67eada72d2927e2a90364
emilyl <emily.lukas@gmail.com>
2020-10-22 22:35:07 -0700

    pass pic data to text extracting function

--- frontend/pilldex/index.js
+++ frontend/pilldex/index.js
@@ -4,11 +4,12 @@
 
 import {AppRegistry} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import firebase from '@react-native-firebase/app';
 import App from './App';
 import {name as appName} from './app.json';
 
 // Register background handler
-messaging().setBackgroundMessageHandler(async remoteMessage => {
+firebase.messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
