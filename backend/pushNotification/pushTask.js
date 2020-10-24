c84834c6cd92e27a47a85a674c01ce8d9199403d
mpchow <mattchow918@gmail.com>
2020-10-23 21:55:58 -0700

    Minutes

--- backend/pushNotification/pushTask.js
+++ backend/pushNotification/pushTask.js
@@ -4,13 +4,14 @@ const db = require('../db/db');
 const Profile = db.User;
 
 const pushTask = async () => {
-   const currTime = new Date().getHours();
+   const currHour = new Date().getHours();
+   const currMin = new Date().getMinutes();
    const currDay = new Date().getDay();
    let profiles = await Profile.find({});
    
    (await profiles).forEach(profile => {
       profile.schedule[currDay].forEach((pill) => {
-         if(pill.time.getHours() === currTime) {
+         if(pill.time.getHours() === currHour && pill.time.getMinutes() === currMin) {
             notifService.sendNotification(profile.token, `It is time to take ${reminder.name}`);
          }
       });
