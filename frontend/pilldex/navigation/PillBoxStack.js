f1ca234be444ab79c28dc3aac823f7e0c85810ce
francisgodinho <francisgodinho2010@hotmail.com>
2020-10-23 22:17:07 -0700

    pillbox stck

---
+++ frontend/pilldex/navigation/PillBoxStack.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import PillboxScreen from '../screens/PillboxScreen.js';
+import PillInfoScreen from '../screens/PillInfoScreen.js';
+
+const Stack = createStackNavigator();
+
+function PillBoxStack ( ) {
+    return (
+        <Stack.Navigator intialRouteName = 'PillBox'
+                         screenOptions={{
+                             headerShown: false
+                         }}>
+          <Stack.Screen name='PillBox' component={PillboxScreen}/>
+          <Stack.Screen name='PillInfo' component={PillInfoScreen}/>
+        </Stack.Navigator>
+      );
+}
+
+export default PillBoxStack;
