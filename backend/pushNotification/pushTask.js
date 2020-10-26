dcf7c28a22c1041caea40f5d6358bcac3ec8139a
mpchow <mattchow918@gmail.com>
2020-10-26 00:22:53 -0700

    remove console loogs

--- backend/pushNotification/pushTask.js
+++ backend/pushNotification/pushTask.js
@@ -4,21 +4,13 @@ const db = require('../db/db');
 const Profile = db.User;
 
 const pushTask = async () => {
-   const currTime = new Date();
    const currHour = new Date().getHours();
    const currMin = new Date().getMinutes();
    const currDay = new Date().getDay();
    let profiles = await Profile.find({});
-   console.log('Have not entered the forEach yet');
    (await profiles).forEach(profile => {
-      console.log(profile);
-	  console.log("First foreach");
-	  console.log(currHour);
-
       profile.schedule[currDay].forEach((pill) => {
-  	     console.log('Entered the second forEach');
          if(pill.time.getHours() === currHour && pill.time.getMinutes() === currMin) {
-			console.log('ITS TIME TO NOTIFY');
 			const payload = {
 				notification: {
 					title: "Testing pushtask Function!",
