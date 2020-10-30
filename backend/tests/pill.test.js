38d6353d465203a77e3e7a36c209e0dfb7ffa6c5
briellelaw <lawbrielle@gmail.com>
2020-10-29 23:00:53 -0700

    Added a mock function - yet to test

---
+++ backend/tests/pill.test.js
@@ -0,0 +1,12 @@
+const pillRoute = require('./../routes/pills.js');
+
+const testPill = {"userId":"testId", "name":"testPill", "totalQuantity":10, "frequency":2, "frequencyUnit":daily, "dosage":1,
+				  "withFood":true, "withSleep":false, "remaining":5};
+
+const mockGetPills = jest.fn().mockImplementation((pillParams) => {
+	if (pillParams.userId == "testId")
+		return testPill;
+	else
+		return null;
+	
+});
