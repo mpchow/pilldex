f9f1f1894202ae0f8c5513ff49baa053f799c1c2
root <root@ip-172-31-17-131.ca-central-1.compute.internal>
2020-10-31 23:25:17 +0000

    adfad

--- backend/pushNotification/manageSchedule.js
+++ backend/pushNotification/manageSchedule.js
@@ -3,7 +3,7 @@ const Pills = db.Pill;
 const Users = db.User;
 const NormalDistribution = require('normal-distribution');
 const pill = require('../modules/pill');
-const dist = new NormalDistribution();
+const dist = new NormalDistribution.default();
 const {v4: uuidv4 } = require('uuid');
 
 const freqMap = [
