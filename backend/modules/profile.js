4e9f15a350064763ee65401748815a3d8387efa2
mpchow <mattchow918@gmail.com>
2020-10-31 15:50:08 -0700

    Complex logic

--- backend/modules/profile.js
+++ backend/modules/profile.js
@@ -1,4 +1,3 @@
-const createSchedule = require('../pushNotification/manageSchedule').createSchedule;
 const db = require('./../db/db.js'); 
 const Profile = db.User;
 const Pill = db.Pill;
@@ -17,7 +16,6 @@ const create = async (profileParams) => {
 
 const update = async (profileParams) => {
    try {
-      //createSchedule(profileParams.userId);
       await Profile.replaceOne({userId: profileParams.userId}, profileParams);
       return {msg: 'Success'};
    }
@@ -29,7 +27,7 @@ const update = async (profileParams) => {
 const remove = async (profileParams) => {
    try {
       await Profile.deleteOne({userId: profileParams.userId});
-	  await Pill.deleteMany({userId: profileParams.userId});
+	   await Pill.deleteMany({userId: profileParams.userId});
       return {msg: 'Success'};
    }
    catch (error) {
