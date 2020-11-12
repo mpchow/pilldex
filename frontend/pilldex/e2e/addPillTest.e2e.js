d1cdcc73ea9d5b89147b3056ecb96d79fdbc9d94
francisgodinho <francisgodinho2010@hotmail.com>
2020-11-12 03:04:37 -0800

    dasa

--- frontend/pilldex/e2e/addPillTest.e2e.js
+++ frontend/pilldex/e2e/addPillTest.e2e.js
@@ -19,7 +19,7 @@ describe('Add Pill Test', () => {
 
     it ('Should take a picture', async () => {  
         await element(by.text("CONFIRM")).tap();
-        await waitFor(element(by.text("1 - Medication Name"))).toBeVisible().withTimeout(20000);
+        await waitFor(element(by.text("1 - Medication Name"))).toBeVisible().withTimeout(30000);
     });
 
     it ('Catches missing information', async () => {  
@@ -30,25 +30,20 @@ describe('Add Pill Test', () => {
         await element(by.id('Daily')).tap();
         await element(by.id('Sleep-Yes')).tap();
 
-        await element(by.text('Verify')).tap();
-        await expect(element(by.text("Make a selection for food field"))).toBeVisible();
-        await element(by.text('OK')).tap();
-    });
-    
-    it ('Catches NaN input for refill', async () => {  
         await element(by.id('Food-Yes')).tap();
         await element(by.id('Units-Refill')).replaceText('a');
 
-        await element(by.text('Verify')).tap();
+        await element(by.text('VERIFY')).tap();
         await expect(element(by.text("Please enter a valid number of units for refill field"))).toBeVisible();
         await element(by.text('OK')).tap();
     });
+
 
     it ('Catches NaN input for frequency', async () => {  
         await element(by.id('Units-Refill')).replaceText('10');
         await element(by.id('Units-Frequency')).replaceText('a');
 
-        await element(by.text('Verify')).tap();
+        await element(by.text('VERIFY')).tap();
         await expect(element(by.text("Please enter a valid number of units for frequency field"))).toBeVisible();
         await element(by.text('OK')).tap();
     });
@@ -57,7 +52,7 @@ describe('Add Pill Test', () => {
         await element(by.id('Units-Frequency')).replaceText('1');
         await element(by.id('Medication-Name')).replaceText("");
 
-        await element(by.text('Verify')).tap();
+        await element(by.text('VERIFY')).tap();
         await expect(element(by.text("Please enter a valid medication name"))).toBeVisible();
         await element(by.text('OK')).tap();
     });
@@ -65,7 +60,7 @@ describe('Add Pill Test', () => {
     it ('Accepts valid information', async () => {
         await element(by.id('Medication-Name')).replaceText("TestPill");
 
-        await element(by.text('Verify')).tap();
+        await element(by.text('VERIFY')).tap();
         await waitFor(element(by.text("My Pilldex"))).toBeVisible().withTimeout(10000);
     });
 
