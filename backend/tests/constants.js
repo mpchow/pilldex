e4bf280cec91f0598a7654ae88379ea852293a2b
briellelaw <lawbrielle@gmail.com>
2020-11-30 02:16:14 -0800

    More unit tests

--- backend/tests/constants.js
+++ backend/tests/constants.js
@@ -1,17 +1,20 @@
 let constants = {
-
+	// Test Pill with Invalid UserId and Invalid Name
 	testPillInvalidUser : {"userId":"invalidUserId", "name":"invalidUserPill", "totalQuantity":10, "frequency":2, "frequencyUnit":"daily", "dosage":1,
 	   			  "withFood":true, "withSleep":false, "remaining":5},
 	
+	// Test Pill with Valid UserId and Valid name
 	testPillValid: {"userId":"validUserId2", "name":"testPill", "totalQuantity":10, "frequency":2, "frequencyUnit":"daily", "dosage":1,
 	   			  "withFood":true, "withSleep":false, "remaining":5},
 	
+	// Test Pill with Invalid name but Valid UserId
 	testPillInvalidName: {"userId":"validUserId2", "name":"invalidName", "totalQuantity":10, "frequency":2, "frequencyUnit":"daily", "dosage":1,
 	   			  "withFood":true, "withSleep":false, "remaining":5},
 	
 	updatePill : {"userId":"validUserId2", "name":"testPill", "totalQuantity":999, "frequency":2, "frequencyUnit":"daily", "dosage":1,
 	   			  "withFood":true, "withSleep":false, "remaining":5},
 
+	// Test Pill Parameters for POST pills/taken requests
 	takenPillInvalidUser: {"userId":"invalidUserId", "name":"invalidUserPill", "totalQuantity":10, "frequency":2, "frequencyUnit":"daily", "dosage":1,
 	   			  "withFood":true, "withSleep":false, "remaining":5, "timeTaken" : {"day":0, "hour":1, "min":10}},
 
@@ -19,21 +22,145 @@ let constants = {
 
 	takenPillInvalidName : {"userId":"validUserId2", "name":"invalidName", "timeTaken" : {"day":0, "hour":1, "min":10}, "reminderId":"214e5249-a89e-473a-a554-609942a01b5f"},
 
-	testLabelName : {"body":"ACETAMINOPHEN"},
-	testLabelQty : {"body":"QTY: 30"},
-
-	
+	// Test Labels for integration testing
 	testLabel : {"body":"1 Local Pharmacy RX# 0004921—39S CUSTOMER NAME GENERIC RX 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY\n PRESCRIPTION NO. STORE NO.PRESCRIBED BY: A. DOCTOR QTY: 20 NO REFILLS REMAIN PRESCRIBER AUTH REQUIRED 123 RX AVENUE NEW YORK, NY NEW DATE FILLED: 02/05/2019 DISCARD BY: 02/05/2020 (555) 555 -555 five"},
-	
-	testLabel2 : {"body":"ONE Local Pharmacy RX# 0004921—39S 82 ACETAMINOPHEN 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY BEFORE BED WITH FOOD\n PRESCRIPTION NO. STORE NO.PRESCRIBED BY: A. DOCTOR REMAIN PRESCRIBER AUTH REQUIRED 123 RX AVENUE NEW YORK, NY NEW DATE FILLED: 02/05/2019 DISCARD BY: 02/05/2020 (555) 555 -555"},
-	
-	testLabel3 : {"body":"1 Local Pharmacy RX# 0004921—39S five ACETAMINOPHEN 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY BEFORE BED WITH FOOD\n PRESCRIPTION NO. STORE NO.PRESCRIBED BY: A. DOCTOR REMAIN PRESCRIBER AUTH REQUIRED 123 RX AVENUE NEW YORK, NY NEW DATE FILLED: 02/05/2019 DISCARD BY: 02/05/2020 (555) 555 -555"},
-	
-	testLabel4: {"body":"ONE Local Pharmacy RX# 0004921—39S CUSTOMER NAME GENERIC RX 500 MG TABLET\n TAKE 1 TABLET 2 TIMES WEEKLY\n PRESCRIPTION NO. STORE NO.PRESCRIBED BY: A. DOCTOR QTY: FIVE NO REFILLS REMAIN PRESCRIBER AUTH REQUIRED 123 RX AVENUE NEW YORK, NY NEW DATE FILLED: 02/05/2019 DISCARD BY: TIMES"},
+	testLabel2 : {"body":"82 ACETAMINOPHEN 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY BEFORE BED WITH FOOD\n 123 RX AVENUE NEW YORK, NY 555"},
+	testLabel3 : {"body":"1 0004921—39S five ACETAMINOPHEN 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY BEFORE BED WITH FOOD\n "},
+	testLabel4: {"body":"ONE RX 500 MG TABLET\n TAKE 1 TABLET 2 TIMES WEEKLY\n QTY: FIVE BY: TIMES"},
+	testLabel5: {"body":"1 TAKE 1 TABLET TWO TIMES WEEKLY\n QTY: FIVE NO 555 TAKE"},
+	testLabel6 : {"body":"ACETAMINOPHEN 1 Local QTY: 5 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY BEFORE BED WITH FOOD\n QTY"},
 
-	testLabel5: {"body":"1 Local Pharmacy RX# 0004921—39S CUSTOMER NAME GENERIC RX 500 MG TABLET\n TAKE 1 TABLET TWO TIMES WEEKLY\n PRESCRIPTION NO. STORE NO.PRESCRIBED BY: A. DOCTOR QTY: FIVE NO REFILLS REMAIN PRESCRIBER AUTH REQUIRED 123 RX AVENUE NEW YORK, NY NEW DATE FILLED: 02/05/2019 DISCARD BY: 02/05/2020 (555) 555 -555 TAKE"},
+	// Test Labels for Label Unit Testing
+	testLabelName : {"body":"ACETAMINOPHEN"},
+	testLabelMixName: {"body":"ACeTaMInOpheN"},
+	testLabelQty : {"body":"QTY: 10"},
+	testLabelFreqUnit : {"body":"daily"},
+	testLabelMixFreq: {"body":"dAIlY"},
+	testLabelConditions : {"body":"sleep food"},
+	testLabelMixConditions : {"body":"SleEP fOoD"},
+	testLabelSleep: {"body":"sleep"},
+	testLabelEat: {"body":"food"},
+	testLabelDosage : {"body":"2"},
+	testLabelFreq: {"body":"once"},
+	testLabelNQ1: {"body":"10 ACETAMINOPHEN"},
+	testLabelNQ2: {"body":"TEN ACETAMINOPHEN"},
+	testLabelAll1: {"body":"10 ACETAMINOPHEN TAKE 2 ONCE DAILY WITH FOOD AND SLEEP"},
+	testLabelAll2: {"body":"TEN ACETAMINOPHEN TAKE TWO 1 TIME DAILY WITH FOOD AND SLEEP"},
+	testLabelNL: {"body":"10 ACETAMINOPHEN\n TAKE \n 2 \nONCE DAILY\n WITH \n FOOD AND SLEEP"},
+	testLabelSpace : {"body":"10      ACETAMINOPHEN    TAKE      2 ONCE DAILY WITH FOOD AND    SLEEP"},
+	testLabelBounds: {"body":"TEN ACETAMINOPHEN TAKE TWO 1 TIME DAILY WITH FOOD AND SLEEP QTY"},
+	testLabelNone: {"body":"PRESCRIBED ** BY A DOCTOR WITHIN 8 MINUTES YOU MAY NEED TO CALL"},
+	
+	// Parsed Label results for Label Unit Testing
+	parsedLabelName : {
+	        "name": "acetaminophen",
+	        "totalQuantity": null,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
 
-	testLabel6 : {"body":"ACETAMINOPHEN 1 Local Pharmacy RX# 0004921—39S QTY: 5 500 MG TABLET\n TAKE ONE TABLET TWICE DAILY BEFORE BED WITH FOOD\n PRESCRIPTION NO. STORE NO.PRESCRIBED BY: A. DOCTOR REMAIN PRESCRIBER AUTH REQUIRED 123 RX AVENUE NEW YORK, NY NEW DATE FILLED: 02/05/2019 DISCARD BY: 02/05/2020 (555) 555 -555 QTY"},
+	parsedLabelQty : {
+	        "name": null,
+	        "totalQuantity": 10,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
+
+	parsedLabelFreqUnit : {
+	        "name": null,
+	        "totalQuantity": null,
+	        "frequency": null,
+	        "frequencyUnit": "daily",
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
+
+	parsedLabelConditions: {
+	        "name": null,
+	        "totalQuantity": null,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": true,
+	        "withSleep": true 
+	},
+
+	parsedLabelSleep: {
+	        "name": null,
+	        "totalQuantity": null,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": true 
+	},
+
+	parsedLabelEat: {
+	        "name": null,
+	        "totalQuantity": null,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": true,
+	        "withSleep": false 
+	},
+
+	parsedLabelDosage : {
+	        "name": null,
+	        "totalQuantity": null,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": 2, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
+
+	parsedLabelFreq : {
+	        "name": null,
+	        "totalQuantity": null,
+	        "frequency": 1,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
+
+	parsedLabelNQ1: {
+	        "name": "acetaminophen",
+	        "totalQuantity": 10,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
+
+	parsedLabelNQ2: {
+	        "name": "acetaminophen",
+	        "totalQuantity": 10,
+	        "frequency": null,
+	        "frequencyUnit": null,
+	        "dosage": null, 
+	        "withFood": false,
+	        "withSleep": false 
+	},
+
+	parsedLabelAll: {
+	        "name": "acetaminophen",
+	        "totalQuantity": 10,
+	        "frequency": 1,
+	        "frequencyUnit": "daily",
+	        "dosage": 2,
+	        "withFood": true,
+	        "withSleep": true 
+	},
 
 	parsedLabelNull : {
 	        "name": null,
@@ -84,25 +211,6 @@ let constants = {
 	        "withSleep": false 
 	},
 
-	parsedLabelName : {
-	        "name": "acetaminophen",
-	        "totalQuantity": null,
-	        "frequency": null,
-	        "frequencyUnit": null,
-	        "dosage": null, 
-	        "withFood": false,
-	        "withSleep": false 
-	},
-
-	parsedLabelQty : {
-	        "name": null,
-	        "totalQuantity": 30,
-	        "frequency": null,
-	        "frequencyUnit": null,
-	        "dosage": null, 
-	        "withFood": false,
-	        "withSleep": false 
-	},
 
 	// Expected error results
 	userNotFound : {status: 404, msg: "User Not Found"},
--- backend/tests/label.test.js
+++ backend/tests/label.test.js
@@ -15,5 +15,119 @@ describe("Parse Label Unit Testing", () => {
 		expect(res.pillData).toStrictEqual(Constants.parsedLabelQty);
 		done();
 	})
+	test('Test Parsing FreqUnit', async done => {
+		res = await parser.parseLabel(Constants.testLabelFreqUnit);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelFreqUnit);
+		done();
+	})
+	test('Test Parsing Sleep and Eat conditions', async done => {
+		res = await parser.parseLabel(Constants.testLabelConditions);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelConditions);
+		done();
+	})
+	test('Test Parsing Sleep and NOT Eat conditions', async done => {
+		res = await parser.parseLabel(Constants.testLabelSleep);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelSleep);
+		done();
+	})
+	test('Test Parsing Eat and NOT Sleep conditions', async done => {
+		res = await parser.parseLabel(Constants.testLabelEat);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelEat);
+		done();
+	})
+	test('Test Parsing Dosage', async done => {
+		res = await parser.parseLabel(Constants.testLabelDosage);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelDosage);
+		done();
+	})
+	test('Test Parsing Frequency', async done => {
+		res = await parser.parseLabel(Constants.testLabelFreq);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelFreq);
+		done();
+	})
+	test('Test Parsing empty', async done => {
+		res = await parser.parseLabel("");
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelNull);
+		done();
+	})
+	test('Test Parsing qty before name with digit', async done => {
+		res = await parser.parseLabel(Constants.testLabelNQ1);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelNQ1);
+		done();
+	})
+	test('Test Parsing qty before name with word number', async done => {
+		res = await parser.parseLabel(Constants.testLabelNQ2);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelNQ2);
+		done();
+	})
+	test('Test Parsing all fields with digits', async done => {
+		res = await parser.parseLabel(Constants.testLabelAll1);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelAll);
+		done();
+	})
+	test('Test Parsing all fields with word numbers', async done => {
+		res = await parser.parseLabel(Constants.testLabelAll2);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelAll);
+		done();
+	})
+	test('Test Parsing with Invalid index bounds for name', async done => {
+		res = await parser.parseLabel(Constants.testLabelName);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelName);
+		done();
+	})
+	test('Test Parsing with Invalid index bounds for dosage', async done => {
+		res = await parser.parseLabel(Constants.testLabelBounds);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelAll);
+		done();
+	})
+	test('Test Parsing with non-useful label', async done => {
+		res = await parser.parseLabel(Constants.testLabelNone);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelNull);
+		done();
+	})
+	test('Test Parsing with mix-cased name', async done => {
+		res = await parser.parseLabel(Constants.testLabelMixName);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelName);
+		done();
+	})
+	test('Test Parsing with mix-cased conditions', async done => {
+		res = await parser.parseLabel(Constants.testLabelMixConditions);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelConditions);
+		done();
+	})
+	test('Test Parsing with mix-cased frequency unit', async done => {
+		res = await parser.parseLabel(Constants.testLabelMixFreq);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelFreqUnit);
+		done();
+	})
+	test('Test Parsing with new lines', async done => {
+		res = await parser.parseLabel(Constants.testLabelNL);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelAll);
+		done();
+	})
+	test('Test Parsing with extra spacing', async done => {
+		res = await parser.parseLabel(Constants.testLabelNL);
+		console.log(res);
+		expect(res.pillData).toStrictEqual(Constants.parsedLabelAll);
+		done();
+	})
 });
 
